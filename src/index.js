80c05376f1e05036e55b21502e62c5859d3041ca
Alexander <alexfrysz@gmail.com>
2021-01-10 16:19:41 -0300

    primera demo

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+//configuracion
+app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+//rutas
+app.use(require("./routes/"));
+
+//archivos estaticos
+app.use(express.static(path.join(__dirname, "public/")));
+
+//modulos de node
+app.use('/scripts/', express.static(path.join(__dirname, "./node_modules/")));
+
+//puerto
+app.listen(app.get("port"), () => {
+  console.log("Server run on Port: ", app.get("port"));
+});
